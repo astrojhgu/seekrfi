85e110dc22d17763cd3c2994e4a7dae86aef819a
Gu Junhua <jhgu@nao.cas.cn>
2019-11-20 20:06:54 +0800

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/lib.rs 	modified:   src/main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,112 @@
 #[macro_use]
 extern crate ndarray;
 extern crate num_traits;
+extern crate imageproc;
+extern crate image;
+extern crate statistical;
+extern crate fitsimg;
+
 use num_traits::{zero, one};
-use ndarray::{Array2, Array1};
+use ndarray::{Array2, Array1, ArrayView2};
+use image::ImageBuffer;
+use image::Luma;
+use imageproc::definitions::Clamp;
+use statistical::median;
+use fitsimg::write_img;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum FlagState{
+    Normal,
+    Flagged,
+}
+
+impl Default for FlagState{
+    fn default()->FlagState{
+        FlagState::Normal
+    }
+}
+
+fn flag_if_both_flagged(f1:FlagState, f2:FlagState)->FlagState{
+    if f1==FlagState::Flagged && f2==FlagState::Flagged{
+        FlagState::Flagged
+    }else{
+        FlagState::Normal
+    }
+}
+
+fn flag_if_either_flagged(f1:FlagState, f2:FlagState)->FlagState {
+    if f1==FlagState::Flagged || f2==FlagState::Flagged{
+        FlagState::Flagged
+    }else{
+        FlagState::Normal
+    }
+}
+
+fn flag_if(b:bool)->FlagState {
+    if b{
+        FlagState::Flagged
+    }else{
+        FlagState::Normal
+    }
+}
+
+impl std::ops::BitXor for FlagState{
+    type Output=FlagState;
+
+    fn bitxor(self, rhs:FlagState)->FlagState{
+        if self==rhs{
+            FlagState::Normal
+        }else{
+            FlagState::Flagged
+        }
+    }
+}
+
+impl std::ops::Not for FlagState{
+    type Output=FlagState;
+
+    fn not(self)->FlagState{
+        match self{
+            FlagState::Flagged=>FlagState::Normal,
+            _=>FlagState::Flagged
+        }
+    }
+}
+
+impl std::ops::BitOr for FlagState{
+    type Output=FlagState;
+
+    fn bitor(self, rhs:FlagState)->FlagState{
+        if self==FlagState::Flagged || rhs==FlagState::Flagged{
+            FlagState::Flagged
+        }else{
+            FlagState::Normal
+        }
+    }
+}
+
+pub fn write_mask(mask:&Array2<FlagState>, fname:&str){
+    let m=mask.map(|&x|{
+        match x{
+            FlagState::Flagged=>1,
+            _=>0
+        }
+    });
+
+    write_img(fname.to_string(), &m.into_dyn());
+}
+
+pub fn write_data<T>(img:&Array2<T>, fname:&str)
+where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage
+{
+    let img=img.clone().into_dyn();
+
+    write_img(fname.to_string(), &img);
+}
 
 
-
-pub fn gaussian_filter<T>(v:Array2<T>, mask:&Array2<bool>, kernel_x:usize, kernel_y:usize, sigma_x:T, sigma_y:T)->Array2<T>
-where T:num_traits::Float+std::fmt::Debug
+pub fn gaussian_filter<T>(v:&Array2<T>, mask:&Array2<FlagState>, kernel_x:usize, kernel_y:usize, sigma_x:T, sigma_y:T)->Array2<T>
+where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage
 {
     let two:T=one::<T>()+one::<T>();
     let wd=|n:isize,m:isize|->T{
@@ -18,7 +117,12 @@ where T:num_traits::Float+std::fmt::Debug
     vp.slice_mut(s![kernel_y/2..vp.rows()-kernel_y/2, kernel_x/2..vp.cols()-kernel_x/2]).assign(&v);
 
     let mut wfp=Array2::zeros((v.rows()+kernel_y, v.cols()+kernel_x));
-    wfp.slice_mut(s![kernel_y/2..wfp.rows()-kernel_y/2, kernel_x/2.. wfp.cols()-kernel_x/2]).assign(&mask.map(|&x|{if x {one()} else {zero()}}));
+    wfp.slice_mut(s![kernel_y/2..wfp.rows()-kernel_y/2, kernel_x/2.. wfp.cols()-kernel_x/2]).assign(&mask.map(|&x|{
+        match x{
+            FlagState::Flagged=>zero(),
+            FlagState::Normal=>one()
+        }
+    }));
     let kernel_0=(-(kernel_y as isize)/2..kernel_y as isize/2+1).map(|n|wd(n,0)).collect::<Array1<T>>();
     let kernel_1=(-(kernel_x as isize)/2..kernel_x as isize/2+1).map(|m|wd(0,m)).collect::<Array1<T>>();
     println!("{:?}",kernel_1);
@@ -29,35 +133,179 @@ where T:num_traits::Float+std::fmt::Debug
 }
 
 fn run_gaussian_filter<T>(vp:&Array2<T>, vs0:usize, vs1:usize,
-                          wfp:Array2<T>, mask:&Array2<bool>,
+                          wfp:Array2<T>, mask:&Array2<FlagState>,
                           kernel_0:Array1<T>, kernel_1:Array1<T>, nx:usize, ny:usize)->Array2<T>
-where T:num_traits::Float+std::fmt::Debug{
+where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage{
     let mut vh=Array2::<T>::zeros((vp.rows(), vp.cols()));
     let mut vh2=Array2::<T>::zeros((vp.rows(), vp.cols()));
     let nx2=nx/2;
     let ny2=ny/2;
     for i in ny2..vs0+ny2{
         for j in nx2..vs1+nx2{
-            if !mask[(i-ny2, j-nx2)]{
-                vh[(i,j)]=zero();
-            }else{
-                let val=(&wfp.slice(s![i-ny2..i+ny2+1, j])* &vp.slice(s![i-ny2..i+ny2+1, j])* &kernel_0).sum();
-                vh[(i,j)]=val/(&wfp.slice(s![i-ny2..i+ny2+1, j])* &kernel_0).sum();
-
+            match mask[(i-ny2,j-nx2)]{
+                FlagState::Flagged=>{vh[(i,j)]=zero();},
+                _=>{
+                    let val=(&wfp.slice(s![i-ny2..i+ny2+1, j])* &vp.slice(s![i-ny2..i+ny2+1, j])* &kernel_0).sum();
+                    vh[(i,j)]=val/(&wfp.slice(s![i-ny2..i+ny2+1, j])* &kernel_0).sum();
+                }
             }
         }
     }
 
     for j in nx2..vs1+nx2{
         for i in ny2..vs0+ny2{
-            if !mask[(i-ny2, j-nx2)]{
-                vh2[(i,j)]=zero();
-            }else{
-                let val=(&wfp.slice(s![i, j-nx2..j+nx2+1])*&vh.slice(s![i, j-nx2..j+nx2+1])*&kernel_1).sum();
-                vh2[(i,j)]=val/((&wfp.slice(s![i, j-nx2..j+nx2+1])*&kernel_1)).sum();
-
+            match mask[(i-ny2, j-nx2)]{
+                FlagState::Flagged=>{
+                    vh2[(i,j)]=zero();
+                },
+                _=>{
+                    let val=(&wfp.slice(s![i, j-nx2..j+nx2+1])*&vh.slice(s![i, j-nx2..j+nx2+1])*&kernel_1).sum();
+                    vh2[(i,j)]=val/((&wfp.slice(s![i, j-nx2..j+nx2+1])*&kernel_1)).sum();
+                }
             }
         }
     }
     vh2
+}
+
+
+pub fn binary_mask_dialation(mask:Array2<FlagState>, ss0:usize, ss1:usize, iter:usize)->Array2<FlagState>{
+    let width=mask.cols();
+    let height=mask.rows();
+    let mut img=
+        ImageBuffer::<Luma<u8>, Vec<u8>>::from_raw(
+            width as u32, height as u32,
+            mask.into_shape((width*height,)).unwrap().to_vec().into_iter().map(|x|{
+                match x{
+                    FlagState::Flagged=>1,
+                    _=>0
+                }
+            }).collect::<Vec<_>>()).unwrap();
+
+
+    for i in 0..iter{
+        img=imageproc::filter::separable_filter(&img, &vec![1_u16; ss1], &vec![1_u16; ss0]);
+    }
+    //let img=imageproc::filter::box_filter(&img, 5,5);
+
+    Array1::from_iter(img.into_vec().into_iter().map(|pix|{
+        if pix==0 {
+            FlagState::Normal
+        }else{
+            FlagState::Flagged
+        }
+    })).into_shape((height, width)).unwrap()
+}
+
+pub fn normalize<T>(mut data:Array2<T>, mask:&Array2<FlagState>)->Array2<T>
+where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage{
+    for i in 0..data.rows(){
+        let r:Vec<_>=data.row(i).iter().zip(mask.row(i)
+            .iter())
+            .filter(|(&a,&b)|{b==FlagState::Normal}).map(|(&a,b)|{a}).collect();
+        let med=median(&r);
+        data.row_mut(i).iter_mut().for_each(|x|{*x=(*x-med).abs()});
+    }
+    data
+}
+
+
+pub fn _sumthreshold<T>(data:&ArrayView2<T>, mask:Array2<FlagState>, i:usize, chi:T)->Array2<FlagState>
+where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage{
+    let mut tmp_mask=mask.clone();
+    let ds0=data.rows();
+    let ds1=data.cols();
+    for x in 0..ds0{
+        let mut sum=zero::<T>();
+        let mut cnt=0;
+
+        for ii in 0..i{
+            if mask[(x, ii)]==FlagState::Normal{
+                sum=sum+data[(x, ii)];
+                cnt+=1;
+            }
+        }
+
+        for y in i..ds1{
+            if sum> chi*T::from(cnt).unwrap(){
+                for ii2 in 0..i{
+                    tmp_mask[(x, y-ii2-1)]=FlagState::Flagged;
+                }
+                //tmp_mask.slice_mut(s![x, (y-i)..(y-1)]).fill(FlagState::Flagged);
+            }
+
+            if mask[(x,y)]==FlagState::Normal{
+                sum=sum+data[(x,y)];
+                cnt+=1;
+            }
+
+            if mask[(x, y-i)]==FlagState::Normal{
+                sum=sum-data[(x,y-i)];
+                cnt-=1;
+            }
+        }
+        println!("{:?} {}", sum, cnt);
+        //panic!();
+    }
+    tmp_mask
+}
+
+pub fn _run_sumthreshold<T>(data:&Array2<T>, init_mask:&Array2<FlagState>,
+                            eta:T, M:&[usize], chi_i:&[T],
+                            kernel_m:usize, kernel_n:usize, sigma_m:T, sigma_n:T)->Array2<FlagState>
+    where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage{
+    let smoothed_data=gaussian_filter(data, init_mask, kernel_m, kernel_n, sigma_m, sigma_n);
+    let res=data-&smoothed_data;
+    let mut st_mask=init_mask.clone();
+    for (&m, &chi) in M.iter().zip(chi_i.iter()){
+        let chi=chi/eta;
+        if m==1{
+            st_mask.zip_mut_with(&res, |a, &b|{
+                if b>chi{
+                    *a=FlagState::Flagged;
+                }
+            });
+        }else{
+            write_img("res.fits".to_string(), &res.clone().into_dyn());
+            write_mask( &st_mask.clone(),"mask0.fits");
+            println!("{} {:?}", m, chi);
+            st_mask=_sumthreshold(&res.view(), st_mask, m, chi);
+            write_mask(&st_mask, &format!("mask_{:?}_{}_1.fits", eta, m));
+            panic!();
+            st_mask=_sumthreshold(&res.t(), st_mask.t().to_owned(), m, chi).t().to_owned();
+            write_mask(&st_mask, &format!("mask_{:?}_{}_2.fits", eta, m));
+
+        }
+    }
+    st_mask
+}
+
+
+pub fn get_rfi_mask<T>(data1:&Array2<T>, mask1:Option<&Array2<FlagState>>, chi_1:T, eta_i:&[T], normalize_standing_wave:bool, suppress_dialation:bool,
+kernel_m:usize, kernel_n:usize, sigma_m:T, sigma_n:T, di_args:(usize, usize))->Array2<FlagState>
+where T:num_traits::Float+std::fmt::Debug+fitsimg::TypeToImageType+fitsio::images::WriteImage{
+    let mask=mask1
+        .map_or(Array2::<FlagState>::default((data1.rows(), data1.cols())), |x|{x.clone()});
+    let mut data=data1.clone();
+    if normalize_standing_wave{
+        data=normalize(data, &mask);
+    }
+
+    write_data(&data, "normalized.fits");
+    let two=one::<T>();
+    let p=T::from(1.5).unwrap();
+    let M:Vec<_>=(1..8).map(|m| (2_usize.pow(m-1))).collect();
+    let chi_i:Vec<_>=M.iter().map(|&m|{chi_1/p.powf(T::from(m).unwrap().log2())}).collect();
+    let mut st_mask=mask;
+    for &eta in eta_i{
+        st_mask=_run_sumthreshold(&data, &st_mask, eta, &M[..], &chi_i, kernel_m, kernel_n, sigma_m, sigma_n);
+    }
+    write_mask(&st_mask, "st_mask.fits");
+    if suppress_dialation{
+        st_mask
+    }else{
+        binary_mask_dialation(
+            mask1.map_or(
+                st_mask.clone(), |x|{!(x^&st_mask)}), di_args.0, di_args.1, 2)|st_mask
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ extern crate seekrfi;
 use ndarray::{Array2, ArrayD};
 use fitsimg::write_img;
 use seekrfi::gaussian_filter;
-
+use seekrfi::FlagState;
 
 fn main() {
     let mut mat=Array2::<f64>::zeros((1024,1024));
 
     mat[(512,512)]=1.0;
 
-    let mut mask=!Array2::<bool>::default((1024,1024));
-    //mask[(512,512)]=true;
+    let mut mask=Array2::<FlagState>::default((1024,1024));
+    mask[(512,512)]=FlagState::Flagged;
 
-    let mat1=gaussian_filter(mat, &mask, 16,32,4.0, 4.0);
+    let mat1=gaussian_filter(&mat, &mask, 16,32,4.0, 4.0);
 
     write_img("./a.fits".to_string(), &mat1.into_dyn());
+
+
+    let mut mask=Array2::<FlagState>::default((32,32));
+    mask[(16,16)]=FlagState::Flagged;
+
+    let mask=seekrfi::binary_mask_dialation(mask, 3,3,1).map(|&x|{x as i32});
+
+    write_img("./b.fits".to_string(), &mask.into_dyn());
 }
